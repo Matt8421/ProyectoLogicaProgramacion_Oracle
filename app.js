be9a121393722a08d0fb1d7d89d2926416475283
Matt <matiaslucero21884@gmail.com>
2024-07-08 17:44:16 -0300

    Agregamos Parseint en js

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-let titulo = document.querySelector("h1")
+/* let titulo = document.querySelector("h1")
 titulo.innerHTML = "Juego del número secreto";
+ */
+/* let parrafo = document.querySelector("p");
+parrafo.innerHTML =""; */
 
-let parrafo = document.querySelector("p");
-parrafo.innerHTML ="Indica un número del 1 al 10";
+let numeroSecreto = generarNumeroSecreto();
+
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML = document.querySelector(elemento)
+    elementoHTML.innerHTML = texto;
+    return;
+}
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value) ;
+    console.log(typeof(numeroDeUsuario));
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto);
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random()*10) + 1;
+    
+}
+asignarTextoElemento("h1","Juego del número secreto!");
+asignarTextoElemento("p","Indica un número del 1 al 100");
